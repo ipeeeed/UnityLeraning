4d02d202cd1599836d57df4231a6281cdfb9a134
Pedro Matos <pedro_matos2012@hotmail.com>
2024-10-31 23:20:32 -0400

    Update KeysMouseButtons.cs

--- Estudos/Assets/Scripts/KeysMouseButtons.cs
+++ Estudos/Assets/Scripts/KeysMouseButtons.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class KeysMouseButtons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // OnMouse - Conjunto de métodos de interação do mouse.
+    private void OnMouseDown()
+    {
+        Debug.Log("Clicou");
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Input - Usada para interações do jogador.
+        // GetKey - Capta cada frame com a tecla pressionada.
+        // GetKeyUp - Capta uma vez a tecla ao pressionar.
+        // GetKeyDown - Capta uma vez a tecla após pressionar.
+        // KeyCode - Usada para definir qual tecla do teclado será usada.
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            Debug.Log("Pulando");
+        }
+        if (Input.GetKeyUp(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            Debug.Log("Agachando");
+        }
+        if (Input.GetKeyUp(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Direita");
+        }
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Esquerda");
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Atira");
+        }
+
+        if (Input.GetKey(KeyCode.O))
+        {
+            Debug.Log("Carrega Arma");
+        }
     }
 }
